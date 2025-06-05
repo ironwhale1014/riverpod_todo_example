23a25c8e2d733c977bc8ce004333f0d6ab225a73
King <mac@mac.com>
2025-06-06 00:45:31 +0900

    crud

---
+++ lib/screen/components/todo_card.dart
@@ -0,0 +1,68 @@
+import 'package:drift_todo_train/database/repository_provider.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+class TodoCard extends HookConsumerWidget {
+  const TodoCard({super.key, required this.todo});
+
+  final TodoWithCategory todo;
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final editController = useTextEditingController();
+    final tileFocus = useFocusNode();
+    final textFieldFocus = useFocusNode();
+    final isFocused = useIsFocused(tileFocus);
+    return Focus(
+      focusNode: tileFocus,
+      onFocusChange: (hasFocus) {
+        if (hasFocus) {
+          editController.text = todo.todoEntry.description;
+        } else {
+          ref
+              .read(repositoryProvider.notifier)
+              .updateTodos(
+                todo: todo.todoEntry.copyWith(description: editController.text),
+              );
+        }
+      },
+      child: ListTile(
+        onTap: () {
+          tileFocus.requestFocus();
+          textFieldFocus.requestFocus();
+        },
+        title: (isFocused)
+            ? TextField(
+                controller: editController,
+                focusNode: textFieldFocus,
+                autofocus: true,
+              )
+            : Text(todo.todoEntry.description),
+        trailing: IconButton(
+          onPressed: () {
+            ref
+                .read(repositoryProvider.notifier)
+                .deleteTodos(todo: todo.todoEntry);
+          },
+          icon: Icon(Icons.delete),
+        ),
+      ),
+    );
+  }
+}
+
+bool useIsFocused(FocusNode value) {
+  final isFocused = useState(value.hasFocus);
+  useEffect(() {
+    void listener() {
+      isFocused.value = value.hasFocus;
+    }
+
+    value.addListener(listener);
+    return () => value.removeListener(listener);
+  }, [value]);
+
+  return isFocused.value;
+}
--- lib/screen/home.dart
+++ lib/screen/home.dart
@@ -1,75 +1,50 @@
 import 'package:drift_todo_train/database/repository_provider.dart';
 import 'package:drift_todo_train/database/state.dart';
+import 'package:drift_todo_train/screen/components/todo_card.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-class HomePage extends ConsumerStatefulWidget {
+class HomePage extends HookConsumerWidget {
   const HomePage({super.key});
 
   @override
-  ConsumerState createState() => _HomePageState();
-}
+  Widget build(BuildContext context, WidgetRef ref) {
+    final controller = useTextEditingController();
 
-class _HomePageState extends ConsumerState<HomePage> {
-  final _controller = TextEditingController();
-
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
     final todos = ref.watch(todoWithCategoryProvider);
     return Scaffold(
       appBar: AppBar(title: Text("home", textAlign: TextAlign.center)),
-      body: Column(
-        children: [
-          TextFormField(
-            controller: _controller,
-            onFieldSubmitted: (value) {
-              ref
-                  .read(repositoryProvider.notifier)
-                  .saveTodos(description: _controller.text);
-              _controller.clear();
-            },
-          ),
-          Expanded(
-            child: todos.when(
-              data: (todos) => ListView.builder(
-                itemCount: todos.length,
-                itemBuilder: (context, index) => ListTile(
-                  onTap: () {
-                    ref
-                        .read(repositoryProvider.notifier)
-                        .updateTodos(
-                          todo: todos[index].todoEntry.copyWith(
-                            description: "new description",
-                          ),
-                        );
-                  },
-                  title: Text(todos[index].todoEntry.description),
-                  trailing: IconButton(
-                    onPressed: () {
-                      ref
-                          .read(repositoryProvider.notifier)
-                          .deleteTodos(todo: todos[index].todoEntry);
-                    },
-                    icon: Icon(Icons.delete),
-                  ),
-                ),
-              ),
-              error: (_, _) => Text("error"),
-              loading: () {
-                return const Align(
-                  alignment: Alignment.center,
-                  child: CircularProgressIndicator(),
-                );
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16),
+        child: Column(
+          children: [
+            TextFormField(
+              controller: controller,
+              onFieldSubmitted: (value) {
+                ref
+                    .read(repositoryProvider.notifier)
+                    .saveTodos(description: controller.text);
+                controller.clear();
               },
             ),
-          ),
-        ],
+            Expanded(
+              child: todos.when(
+                data: (todos) => ListView.builder(
+                  itemCount: todos.length,
+                  itemBuilder: (context, index) => TodoCard(todo: todos[index]),
+                ),
+                error: (_, _) => Text("error"),
+                loading: () {
+                  return const Align(
+                    alignment: Alignment.center,
+                    child: CircularProgressIndicator(),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
