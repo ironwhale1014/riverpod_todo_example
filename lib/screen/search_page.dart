76f3324d51f19ebd8d920b98cf4802c33d3b719c
King <mac@mac.com>
2025-06-05 12:34:25 +0900

    base

---
+++ lib/screen/search_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+class SearchPage extends ConsumerStatefulWidget {
+  const SearchPage({super.key});
+
+  @override
+  ConsumerState createState() => _SearchPageState();
+}
+
+class _SearchPageState extends ConsumerState<SearchPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
