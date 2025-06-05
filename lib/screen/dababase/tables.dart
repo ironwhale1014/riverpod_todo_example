ecc6fd20543d5d904a961a911348bd49371be892
King <mac@mac.com>
2025-06-05 12:47:11 +0900

    스키마 1 테이블

---
+++ lib/screen/dababase/tables.dart
@@ -0,0 +1,31 @@
+import 'dart:ui';
+
+import 'package:drift/drift.dart';
+
+mixin AutoIncrementPrimaryKey on Table {
+  IntColumn get id => integer().autoIncrement()();
+}
+
+@DataClassName('TodoEntry')
+class Todos extends Table with AutoIncrementPrimaryKey {
+  TextColumn get description => text()();
+}
+
+@DataClassName('Category')
+class Categories extends Table with AutoIncrementPrimaryKey {
+  TextColumn get name => text()();
+
+  IntColumn get color => integer().map(ColorConvertor())();
+}
+
+class ColorConvertor extends TypeConverter<Color, int> {
+  @override
+  Color fromSql(int fromDb) {
+    return Color(fromDb);
+  }
+
+  @override
+  int toSql(Color value) {
+    return value.toARGB32();
+  }
+}
