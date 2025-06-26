4a56f44857b9c592108e41980bcace0e537d854e
King <mac@mac.com>
2025-06-26 12:36:08 +0900

    home_page input todo

--- lib/screen/home_page.dart
+++ lib/screen/home_page.dart
@@ -1,4 +1,6 @@
 import 'package:drift_todo_train/common/layout/default_layout.dart';
+import 'package:drift_todo_train/database/database.dart';
+import 'package:drift_todo_train/database/todo_service.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
@@ -13,7 +15,39 @@ class HomePage extends ConsumerWidget {
       title: 'Home Page',
       child: Column(
         children: [
-          TextFormField(controller: controller, onFieldSubmitted: (_) {}),
+          TextFormField(
+            controller: controller,
+            onFieldSubmitted: (_) {
+              if (controller.text.isNotEmpty) {
+                ref
+                    .read(todoServiceProvider.notifier)
+                    .todoSave(description: controller.text.trim());
+                controller.clear();
+              }
+            },
+          ),
+          Expanded(
+            child: StreamBuilder(
+              stream: ref.watch(todoServiceProvider.notifier).getTodoEntries(),
+              builder: (context, snapshot) {
+                if (snapshot.hasData) {
+                  final List<TodoEntry> todoEntries = snapshot.data!;
+
+                  return ListView.builder(
+                    itemCount: todoEntries.length,
+                    itemBuilder: (context, index) {
+                      final todoEntry = todoEntries[index];
+                      return ListTile(
+                        title: Text(todoEntry.description),
+                        trailing: Text(todoEntry.dueDate?.toString() ?? 'null'),
+                      );
+                    },
+                  );
+                }
+                return Text("no data");
+              },
+            ),
+          ),
         ],
       ),
     );
