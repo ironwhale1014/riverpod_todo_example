10c73739828a75e6728116d3ad93fdc742687062
King <mac@mac.com>
2025-06-26 12:55:49 +0900

    addColumn으로 하자

--- lib/database/database.dart
+++ lib/database/database.dart
@@ -1,8 +1,11 @@
 import 'package:drift/drift.dart';
 import 'package:drift/native.dart';
 import 'package:drift_todo_train/common/util/get_db_file.dart';
+import 'package:drift_todo_train/database/database.steps.dart';
 import 'package:drift_todo_train/database/tables.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+import '../common/util/logger.dart';
 
 part 'database.g.dart';
 
@@ -27,4 +30,23 @@ class AppDatabase extends _$AppDatabase {
 
   @override
   int get schemaVersion => 2;
+
+  @override
+  MigrationStrategy get migration {
+    return MigrationStrategy(
+      onUpgrade: stepByStep(
+        from1To2: (m, schema) async {
+          logger.d('from1To2');
+          await m.addColumn(schema.todoEntries, schema.todoEntries.category);
+          await m.alterTable(TableMigration(schema.todoEntries));
+          logger.d('from1To2 end');
+        },
+      ),
+
+      beforeOpen: (details) async {
+        logger.d(details.versionNow);
+        logger.d(details.versionBefore);
+      },
+    );
+  }
 }
