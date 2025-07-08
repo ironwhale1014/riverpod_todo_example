08b1322cdce50eec0f0804e60fe68d7851112237
King <mac@mac.com>
2025-07-08 12:38:53 +0900

    migration end search function start

--- lib/database/database.dart
+++ lib/database/database.dart
@@ -14,7 +14,7 @@ AppDatabase database(Ref ref) {
   return AppDatabase();
 }
 
-@DriftDatabase(tables: [Categories, TodoEntries],include: {'sql.drift'})
+@DriftDatabase(tables: [Categories, TodoEntries], include: {'sql.drift'})
 class AppDatabase extends _$AppDatabase {
   AppDatabase()
     : super(
@@ -26,7 +26,7 @@ class AppDatabase extends _$AppDatabase {
 
   @override
   // TODO: implement schemaVersion
-  int get schemaVersion => 2;
+  int get schemaVersion => 3;
 
   @override
   MigrationStrategy get migration {
@@ -38,6 +38,17 @@ class AppDatabase extends _$AppDatabase {
           await m.addColumn(schema.todoEntries, schema.todoEntries.isComplete);
           await m.alterTable(TableMigration(schema.todoEntries));
           logger.d('from1To2 end');
+        },
+        from2To3: (Migrator m, Schema3 schema) async {
+          logger.d('from2To3 ');
+          await m.createTable(schema.todoEntriesFts);
+          await customStatement(
+            'INSERT INTO todo_entries_fts(rowid,description) SELECT id, description FROM todo_entries;',
+          );
+          await m.create(schema.todoEntriesInsert);
+          await m.create(schema.todoEntriesDelete);
+          await m.create(schema.todoEntriesUpdate);
+          logger.d('from2To3 end');
         },
       ),
     );
--- lib/database/database.steps.dart
+++ lib/database/database.steps.dart
@@ -117,8 +117,81 @@ i1.GeneratedColumn<bool> _column_6(String aliasedName) =>
       ),
       defaultValue: const CustomExpression('0'),
     );
+
+final class Schema3 extends i0.VersionedSchema {
+  Schema3({required super.database}) : super(version: 3);
+  @override
+  late final List<i1.DatabaseSchemaEntity> entities = [
+    categories,
+    todoEntries,
+    todoEntriesFts,
+    todoEntriesInsert,
+    todoEntriesUpdate,
+    todoEntriesDelete,
+  ];
+  late final Shape0 categories = Shape0(
+    source: i0.VersionedTable(
+      entityName: 'categories',
+      withoutRowId: false,
+      isStrict: false,
+      tableConstraints: [],
+      columns: [_column_0, _column_1, _column_2],
+      attachedDatabase: database,
+    ),
+    alias: null,
+  );
+  late final Shape1 todoEntries = Shape1(
+    source: i0.VersionedTable(
+      entityName: 'todo_entries',
+      withoutRowId: false,
+      isStrict: false,
+      tableConstraints: [],
+      columns: [_column_0, _column_3, _column_4, _column_5, _column_6],
+      attachedDatabase: database,
+    ),
+    alias: null,
+  );
+  late final Shape2 todoEntriesFts = Shape2(
+    source: i0.VersionedVirtualTable(
+      entityName: 'todo_entries_fts',
+      moduleAndArgs:
+          'fts5(description, content=todo_entries, content_rowid=id)',
+      columns: [_column_7],
+      attachedDatabase: database,
+    ),
+    alias: null,
+  );
+  final i1.Trigger todoEntriesInsert = i1.Trigger(
+    'CREATE TRIGGER todo_entries_insert AFTER INSERT ON todo_entries BEGIN INSERT INTO todo_entries_fts ("rowid", description) VALUES (new.id, new.description);END',
+    'todo_entries_insert',
+  );
+  final i1.Trigger todoEntriesUpdate = i1.Trigger(
+    'CREATE TRIGGER todo_entries_update AFTER UPDATE ON todo_entries BEGIN INSERT INTO todo_entries_fts (todo_entries_fts, "rowid", description) VALUES (\'delete\', old.id, old.description);INSERT INTO todo_entries_fts ("rowid", description) VALUES (new.id, new.description);END',
+    'todo_entries_update',
+  );
+  final i1.Trigger todoEntriesDelete = i1.Trigger(
+    'CREATE TRIGGER todo_entries_delete AFTER DELETE ON todo_entries BEGIN INSERT INTO todo_entries_fts (todo_entries_fts, "rowid", description) VALUES (\'delete\', old.id, old.description);END',
+    'todo_entries_delete',
+  );
+}
+
+class Shape2 extends i0.VersionedVirtualTable {
+  Shape2({required super.source, required super.alias}) : super.aliased();
+  i1.GeneratedColumn<String> get description =>
+      columnsByName['description']! as i1.GeneratedColumn<String>;
+}
+
+i1.GeneratedColumn<String> _column_7(String aliasedName) =>
+    i1.GeneratedColumn<String>(
+      'description',
+      aliasedName,
+      false,
+      type: i1.DriftSqlType.string,
+      $customConstraints: '',
+    );
 i0.MigrationStepWithVersion migrationSteps({
   required Future<void> Function(i1.Migrator m, Schema2 schema) from1To2,
+  required Future<void> Function(i1.Migrator m, Schema3 schema) from2To3,
 }) {
   return (currentVersion, database) async {
     switch (currentVersion) {
@@ -127,6 +200,11 @@ i0.MigrationStepWithVersion migrationSteps({
         final migrator = i1.Migrator(database, schema);
         await from1To2(migrator, schema);
         return 2;
+      case 2:
+        final schema = Schema3(database: database);
+        final migrator = i1.Migrator(database, schema);
+        await from2To3(migrator, schema);
+        return 3;
       default:
         throw ArgumentError.value('Unknown migration from $currentVersion');
     }
@@ -135,6 +213,7 @@ i0.MigrationStepWithVersion migrationSteps({
 
 i1.OnUpgrade stepByStep({
   required Future<void> Function(i1.Migrator m, Schema2 schema) from1To2,
+  required Future<void> Function(i1.Migrator m, Schema3 schema) from2To3,
 }) => i0.VersionedSchema.stepByStepHelper(
-  step: migrationSteps(from1To2: from1To2),
+  step: migrationSteps(from1To2: from1To2, from2To3: from2To3),
 );
