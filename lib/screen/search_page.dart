7d233fc53441565657e96698fe05f24d1c146866
King <mac@mac.com>
2025-08-25 12:19:35 +0900

    go_router setting

---
+++ lib/screen/search_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/cupertino.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+class SearchPage extends ConsumerWidget {
+  const SearchPage({super.key});
+
+  static String get routeName => 'SearchPage';
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Container();
+  }
+}
